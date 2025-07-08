2640dcec735d7d7fa1c5864bb90c67cd8584e7bc
isd9194 <sachindangi942@gmail.com>
2025-07-08 18:18:00 +0530

    Ignore ESLint errors during Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Build करते समय ESLint errors को ignore करेगा (Vercel पर helpful)
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
